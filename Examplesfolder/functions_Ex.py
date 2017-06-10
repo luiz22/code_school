0b4076483782477c0f4b63c8984284463f3d51ee
NIS <lewisfiles2005@yahoo.com>
2017-06-10 10:20:04 -0700

    Basic functions

---
+++ Examplesfolder/functions_Ex.py
@@ -0,0 +1,44 @@
+#Functions 
+# they are a block 
+
+
+#A function is a block of organized, reusable code that is used to perform a single, related action. 
+#Functions provide better modularity for your application and a high degree of code reusing.
+
+
+#set arguemnt by default 
+
+#simple form with input from the user
+# def form():
+# 	name = raw_input("what is your name ?")
+# 	age = raw_input ("how old are you? ")
+# 	gender = raw_input("what is your gender? ")
+# 	address = raw_input("where do you stay? ")
+# 	return name,age,address
+
+# print form()
+
+
+#functions with argument
+# def additon(number):
+# 	print number + number
+# 	pass
+# #a=5
+# additon(6)
+
+#set your argument to a default arguemnt 
+# def subtraction(number = 20):
+# 	print number - number
+# 	pass
+# subtraction(7)
+
+# def addition(first,second):
+# 	print first + second
+# 	pass
+
+# addition(6,6)
+
+def add (one):
+	print one + one
+	pass
+add(1)
