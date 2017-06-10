4e4aedc4d9e3e7de3fd70454374d6c21cb9b1093
NIS <lewisfiles2005@yahoo.com>
2017-06-10 10:08:29 -0700

    improved calc

---
+++ Examplesfolder/calc_Improved.py
@@ -0,0 +1,74 @@
+#Simple Calculator
+
+
+#Select your operation symbol
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division 
+''')
+
+
+# val = raw_input("eneter first number")
+# operator = raw_input ("operator eg. +,/,-")
+#Prompts user for an input
+number_1 =int(input("Enter first Value: "))
+number_2 =int(input("Enter Second Value: "))
+
+#print number_1+number_2
+
+
+#string formatters makes it user friendly
+
+#Addition
+if operation == "+":
+	print("{}+{}=" .format(number_1,number_2))
+	print number_1 + number_2
+# if operation == "+":
+# 	print("{} + {}=" .format(number_1,number_2)) 
+# print number_1+number_2
+
+#Subtraction
+elif operation == "-":
+	print("{}+{}=".format(number_1,number_2))
+	print number_1 - number_2
+# elif operation =="-":
+# 	print("{}-{}=".format(number_1,number_2))
+# print number_1 - number_2
+
+#division
+elif operation =="/":
+	print("{}/{}=".format(number_1,number_2))
+	print number_1/number_2
+# elif operation == "/":
+# 	print("{}/{}=".format(number_1,number_2))
+# print number_1 / number_2
+
+#Multiplication
+elif operation == "*":
+	print("{}*{}=".format(number_1,number_2))
+	print number_1 * number_2
+# elif operation == "*":
+# 	print("{}*{}=".format(number_1,number_2))
+# print	number_1 * number_2
+
+#Modula
+elif operation == "%":
+	print("{}+{}=".format(number_1,number_2))
+	print number_1 % number_2
+# elif operation == "%":
+# 	print("{}%{}=".format(number_1,number_2))
+# print number_1 % number_2
+
+#Square
+elif operation == "**":
+	print("{}**{}=".format(number_1,number_2))
+	print number_1 ** number_2
+# elif operation == "**":
+# 	print("{}**{}".format(number_1,number_2))
+# print number_1 ** number_2
+
+else:
+	print("That is not a valid operation, try again!" )
