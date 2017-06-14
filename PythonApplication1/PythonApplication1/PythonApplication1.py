f635261da58b6ec5b20eee44e5fb2dff891010f1
NIS <lewisfiles2005@yahoo.com>
2017-06-14 07:07:51 -0700

    VS

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,48 @@
+
+print("User Friendly Calculator ") 
+
+
+#choice = input("Select a choice from, "+", 
+#Assignement and user keyboard input
+
+
+# val = raw_input("eneter first number")
+# operator = raw_input ("operator eg. +,/,-,%,**")
+
+number_1 =input("Enter first Value: ")
+number_2 =input("Enter second value: ")
+
+#Format string (Addition operator)
+#condition statement
+#if operator == "+":
+print("{}+{}=" .format(number_1,number_2))
+print number_1 + number_2
+
+
+
+#format string (Subtration operator)
+#condition statement
+# elif operator == "-"
+print("{}-{}=" .format(number_1,number_2))
+print number_1 - number_2
+
+#format string (Multiplication)
+#condition statement
+#elif operator == "*":
+print("{}*{}=".format(number_1,number_2))
+print number_1 * number_2
+
+#format string (Modula)
+# elif operator == "%":
+#condition
+print("{}%{}=" .format(number_1,number_2))
+print number_1 % number_2
+
+#format string(raise to the power) 
+#condition statement
+#elif operator == "**":
+print("{}**{}=" .format(number_1,number_2))
+print number_1 ** number_2
+ 
+#else:
+ #   print ("Not and operator, try again")
